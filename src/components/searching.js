4f0139489f581a9951dd03d449be2b796da66270
Devana <vitana.09@yandex.ru>
2026-07-20 20:57:31 +1000

    что то правим

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,20 +1,16 @@
 import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
-    // 1. Создаем свой маленький набор правил (точная копия структуры defaultRules)
-    const searchRules = [
-        {
-            name: searchField,
-            rule: rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)
-        }
-    ];
-    
-    // 2. Добавляем нужную настройку прямо в этот массив 
-    // (в JS массивы являются объектами, поэтому мы можем вешать на них свойства)
-    searchRules.skipEmptyTargetValues = true;
+    // 1. МАГИЯ: Мы добавляем нашу функцию в общий словарь rules под ключом 'search'.
+    // Теперь, когда compare.js сделает rules['search'](), он найдет её!
+    rules[searchField] = rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false);
 
-    // 3. Передаем всё это так же, как это сделано в filtering.js!
-    const compare = createComparison(searchRules);
+    // 2. Передаем массив со строкой (именем поля)
+    // 3. Вторым аргументом передаем настройки, как написано в задании
+    const compare = createComparison(
+        [searchField], 
+        { skipEmptyTargetValues: true }
+    );
 
     return (data, state, action) => {
         return data.filter(row => compare(row, state));
