5b50c134a2ea77a2b71402a661d2e1e426a36851
Devana <vitana.09@yandex.ru>
2026-07-20 20:24:57 +1000

    еще

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,14 +1,21 @@
 import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
-    // @todo: #5.1
+    // @todo: #5.1 — настроить компаратор
     const compare = createComparison(
-        [ rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false) ],
-        { skipEmptyTargetValues: true }
+        // 1. Массив имен полей, которые мы проверяем
+        [searchField], 
+        
+        // 2. Настройки + карта кастомных функций
+        { 
+            skipEmptyTargetValues: true,
+            // Говорим библиотеке: "Если попросят проверить поле searchField, используй эту функцию"
+            [searchField]: rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)
+        }
     );
 
     return (data, state, action) => {
-        // @todo: #5.2
+        // @todo: #5.2 — применить компаратор
         return data.filter(row => compare(row, state));
     }
 }
