a6ac8abe8645f86eb877b90095a4239b15042c34
Devana <vitana.09@yandex.ru>
2026-07-20 20:17:39 +1000

    повтор

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,10 +1,8 @@
 import { rules, createComparison } from "../lib/compare.js";
 
 export function initSearching(searchField) {
-    // Третий параметр true — пропускать пустые значения (skipEmptyTargetValues)
     const searchRule = rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], true);
-    rules[searchField] = searchRule;                // регистрируем правило по имени
-    const Compare = createComparison([searchField]); // передаём только массив имён
-
+    rules[searchField] = searchRule;
+    const Compare = createComparison([searchField]);
     return (data, state, action) => data.filter(row => Compare(row, state));
 }
