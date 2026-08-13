f4ed0d4f0db54e544a0ac66c5a65595b171adeb2
Devana <vitana.09@yandex.ru>
2026-08-13 20:08:57 +1000

    1

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -13,8 +13,8 @@ export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) =
         if (action) switch(action.name) {
             case 'prev': page = Math.max(1, page - 1); break;            
             case 'next': page = Math.min(pageCount, page + 1); break;    
-            case 'first': page = 1; break;                                
-            case 'last': page = pageCount; break;                        
+            case 'first': page = 1; break;
+            case 'last': page = pageCount; break;
         }
 
         return Object.assign({}, query, { 
